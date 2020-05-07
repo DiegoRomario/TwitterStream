f99ea7df724f2961aa329b82a02fa26a1b1f2d5e
Diego Romario <diego.romario@outlook.com>
2020-05-07 14:38:10 -0300

    Consuming Twitter Api by Tweetinvi

--- src/TwitterStream.Producer/Worker.cs
+++ src/TwitterStream.Producer/Worker.cs
@@ -1,8 +1,12 @@
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Tweetinvi;
+using Tweetinvi.Models;
+using Tweetinvi.Streaming;
 
 namespace TwitterStream.Producer
 {
@@ -23,10 +27,28 @@ namespace TwitterStream.Producer
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-               
+                ITwitterCredentials demo = Auth.SetUserCredentials(_options.ConsumerKey, _options.ConsumerSecret, _options.AccessToken, _options.AccessTokenSecret);
+
+                IFilteredStream stream = Stream.CreateFilteredStream();
+
+                stream.AddTrack("covid");
+
+                stream.AddTweetLanguageFilter(LanguageFilter.Portuguese);
+
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                _logger.LogInformation("I'm listening to twitter");
+
+                stream.MatchingTweetReceived += (sender, arguments) =>
+                {
+                    _logger.LogInformation($"Tweet: {arguments.Tweet.Text}");
+                };
+
+                stream.StartStreamMatchingAllConditions();
+
+                await Task.Delay(15000, stoppingToken);
             }
         }
-        
+
 
 
     }
